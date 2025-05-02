1c0cd782443377a0a5b71035c949825118363b9b
isaladwy <isaladwy@gmail.com>
2025-05-02 23:40:58 +0300

    [ADD] app_one: Define module metadata in the manifest file.

--- odoo/custom_addons/app_one/__manifest__.py
+++ odoo/custom_addons/app_one/__manifest__.py
@@ -1,4 +1,12 @@
 {
-    'name': '',
+    'name': 'App_One',
+    'author': 'Islam Aladwy',
+    'version': '18.0.0.1.0',
+    'category': '',
+    'depends': [
+        'base',
+    ],
+    'data': [],
+    'application': True,
 
 }
